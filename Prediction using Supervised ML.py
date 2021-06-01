8a6e9c869d5969222cc11c0823cfa3a410ca30f3
Nidhi-cdac <83115443+Nidhi-cdac@users.noreply.github.com>
2021-06-01 18:26:03 +0530

    Update Prediction using Supervised ML.py

---
+++ Prediction using Supervised ML.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data_load = pd.read_excel('C:/Users/nidhi/Downloads/student_scores - Sheet1.csv')
+print("Successfully imported data into console")
